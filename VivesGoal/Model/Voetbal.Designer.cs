e0864e485a848f68219ba3bc12ea1b4d2286d4b1
AnthonyBroeckaert <r0464343@student.vives.be>
2016-04-17 16:35:57 +0200

    test

--- VivesGoal/Model/Voetbal.Designer.cs
+++ VivesGoal/Model/Voetbal.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\david\Documents\2de jaar\Web Development\VivesGoal\webdevelopment\VivesGoal\Model\Voetbal.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Gebruiker\Documents\GitHub\webdevelopment\VivesGoal\Model\Voetbal.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
