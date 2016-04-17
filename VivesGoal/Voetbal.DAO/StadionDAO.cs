798c3329b73b3f824643a84004d1d7f3a180ba8f
AnthonyBroeckaert <r0464343@student.vives.be>
2016-04-17 18:39:42 +0200

    Aanvulling

---
+++ VivesGoal/Voetbal.DAO/StadionDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Voetbal.DAO
+{
+    public class StadionDAO
+    {
+        public IEnumerable<Stadion> All()
+        {
+            using (var db = new VoetbalClubEntities())
+            {
+                return db.Stadion.ToList();
+            }
+        }
+    }
+}
