5fb4a7c740a49185fbadc61195f940249d325d21
AnthonyBroeckaert <r0464343@student.vives.be>
2016-04-18 13:27:40 +0200

    test

---
+++ VivesGoal/VivesGoal/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Voetbal.Services;
+using Voetbal.DAO;
+
+namespace VivesGoal.Controllers
+{
+    public class TestController : Controller
+    {
+        private StadionService stadionService;
+
+        public TestController()
+        {
+            stadionService = new StadionService();
+        }
+        // GET: Test
+        public ActionResult Index()
+        {
+            var stadions = stadionService.All();
+            return View(stadions);
+        }
+    }
+}
