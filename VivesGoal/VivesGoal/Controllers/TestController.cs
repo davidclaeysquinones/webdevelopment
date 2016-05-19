5724520b32fa934b9faac5ca9d903848902914ef
davidclaeysquinones <davidclaeys@outlook.com>
2016-05-19 22:13:47 +0200

    rzere

--- VivesGoal/VivesGoal/Controllers/TestController.cs
+++ VivesGoal/VivesGoal/Controllers/TestController.cs
@@ -25,53 +25,7 @@ namespace VivesGoal.Controllers
             return View(stadions);
         }
 
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: Beer/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Beer/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Beer/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Beer/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
